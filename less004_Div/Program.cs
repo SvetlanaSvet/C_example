8b38f867e09c07b7792e644e8a5a669db2b97082
svetlanasvet <svetlanasvet.slt@gmail.com>
2023-04-08 18:40:44 +0600

    Random

--- less004_Div/Program.cs
+++ less004_Div/Program.cs
@@ -1,4 +1,7 @@
 ﻿// Вычисление частного двух чисел
-double numberA = 12;
-double numberB = 5;
-Console.WriteLine(numberA / numberB);
+double numberA = new Random().Next(1,10); //случайное число из диапазона 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,10); 
+Console.WriteLine(numberB);
+double result = numberA / numberB;
+Console.WriteLine(result);
