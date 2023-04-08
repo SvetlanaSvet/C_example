a0d0a8fb0693c1e0cdae8d92f6acdb011fbce658
svetlanasvet <svetlanasvet.slt@gmail.com>
2023-04-09 01:33:07 +0600

    dz_1_3

---
+++ dz_1_3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Определить четное число
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if ((a % 2) == 0)
+    Console.WriteLine($"Число {a} четное");
+else 
+    Console.WriteLine($"Число {a} нечетное");
