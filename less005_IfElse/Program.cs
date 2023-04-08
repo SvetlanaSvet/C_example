7827b87b60222ebb23b96ca175a687ccfda7bbdc
svetlanasvet <svetlanasvet.slt@gmail.com>
2023-04-08 20:00:59 +0600

    If-else

---
+++ less005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿// Конструкция If else
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура, это же Маша!");   
+}
+else
+{
+    Console.WriteLine("Привет, " + username);
+}
