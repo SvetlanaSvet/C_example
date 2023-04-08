8daebd2c9b556e376bb0cd19492da5f648c02b00
svetlanasvet <svetlanasvet.slt@gmail.com>
2023-04-09 01:09:14 +0600

    dz_1_1

---
+++ dz_1_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Принимает 2 числа и выдает max
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+    Console.WriteLine($"max {a}");
+else 
+    Console.WriteLine($"max {b}");
