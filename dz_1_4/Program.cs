c8cf94f7d09cfa74c571055463d8c9337c49c9a9
svetlanasvet <svetlanasvet.slt@gmail.com>
2023-04-09 01:59:35 +0600

    dz_1_4

---
+++ dz_1_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Определить четные числа
+
+Console.WriteLine("Введите длину: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+    if (i % 2 == 0)
+        Console.Write(i + " ");
+
+//или так:
+//for (int i = 2; i <= N; i += 2)
+//    Console.Write(i + " ");
