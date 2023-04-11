0d9f6d0666feadfab2e116dfe2c3c6ada30e209f
svetlanasvet <svetlanasvet.slt@gmail.com>
2023-04-11 20:26:49 +0600

    ArrayLibrary

---
+++ less014_ArrayLibrary/Program.cs
@@ -0,0 +1,51 @@
+﻿// Методы: функции и void-процедуры.
+// Отличие в том, что процедура не возвращает значение (нет result).
+
+void FillArray(int[] collection)  //Заполнение массива (fill- наполнение); collection - так назовем массив 
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col) //Вывод массива в консоль (col - так назовем массив)
+{
+    int count = col.Length;  //длина
+    int position = 0;        //индекс
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    
+    while (index < count)
+    {
+        if (collection[index]==find)
+        {
+            position = index;
+            break; //при первом совпадении
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; //создай новый массив из 10 элементов
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
