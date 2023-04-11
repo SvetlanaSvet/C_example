11009d7d995a3f9d424ea98991c5253b4ebbdf35
svetlanasvet <svetlanasvet.slt@gmail.com>
2023-04-11 20:25:20 +0600

    MethodArray

---
+++ less013_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿// Поиск в массиве
+int[] array = { 11, 21, 31, 41, 15, 61, 17, 15, 19 };
+
+int n = array.Length;
+int find = 15;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //если это число встречается не один раз в массиве
+    }
+    index++;
+}
