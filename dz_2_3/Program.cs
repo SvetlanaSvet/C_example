b94a9ef88832d028c18fe2bd2cc0eb4649e79bc4
svetlanasvet <svetlanasvet.slt@gmail.com>
2023-04-11 20:36:17 +0600

    dz_2_3

---
+++ dz_2_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным
+// 6 -> да 
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите номер дня недели: ");
+
+int weeknum = Convert.ToInt32(Console.ReadLine());
+
+if (weeknum >= 1 && weeknum <= 5)
+{
+    Console.Write("Рабочий день");
+}
+else if (weeknum == 6 || weeknum == 7)
+{
+    Console.Write("Выходной день");
+}
+else
+    Console.Write("Неверно указан день недели");
