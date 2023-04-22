b9163227cd08b5ebe012d4e93b97fa5abaa3b12f
svetlanasvet <svetlanasvet.slt@gmail.com>
2023-04-22 14:31:24 +0600

    dz_4_1_Цикл_возведения_в_степень

---
+++ dz_4_1_Цикл_возведения_в_степень/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243
+// 2, 4 -> 16
+
+Console.Write ("Введите число A: ");
+int A = Convert.ToInt32 (Console.ReadLine());
+int numberA = A; 
+
+Console.Write ("Введите число B: ");
+int B = Convert.ToInt32 (Console.ReadLine());
+
+for (int i = 2; i <= B; i++)
+{
+    A = A * numberA;
+}
+
+Console.WriteLine ($"Ответ: {A}");
