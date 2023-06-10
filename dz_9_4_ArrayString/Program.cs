2be6fd378d9bc1d29cecb0b97834188c05c40fa1
svetlanasvet <svetlanasvet.slt@gmail.com>
2023-06-11 00:16:21 +0600

    Update Program - Add ArraySelect

--- dz_9_4_ArrayString/Program.cs
+++ dz_9_4_ArrayString/Program.cs
@@ -26,5 +26,23 @@ void ArrayStrPrint(string[] array)
 }
 
 
+string[] ArraySelect(string[] array1)
+{
+    string[] array2 = new string[array1.Length];
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+    return array2;
+}
+
 string[] array1 = ArrayStrCreate(UserWriteNumber("Задайте длину массива: "));
 ArrayStrPrint(array1);
+
+string[] array2 = ArraySelect(array1);
+ArrayStrPrint(array2);
